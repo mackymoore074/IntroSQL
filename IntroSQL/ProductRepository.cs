2bd6247f973086b69ec781502d50bc7b8a0541f7
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-09-18 11:07:36 -0700

    Add project files.

---
+++ IntroSQL/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSQL
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        //constructor - and will do some setup work for us
+        public ProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        //Create data
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO products (Name, Price, CategoryID) " +
+                                "VALUES (@name, @price, @categoryID);"
+                , new { name = name, price = price, categoryID = categoryID });
+        }
+
+        //Read data
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM products;");
+        }
+
+        //Update data
+        public void UpdateProductName(int productID, string updatedName)
+        {
+            _connection.Execute("UPDATE products SET Name = @updatedName WHERE ProductID = @productID;",
+                new { updatedName = updatedName, productID = productID });
+        }
+
+        //Delete data
+        public void DeleteProduct(int productID)
+        {
+            _connection.Execute("DELETE FROM reviews WHERE ProductID = @productID;",
+                new { productID = productID });
+
+            _connection.Execute("DELETE FROM sales WHERE ProductID = @productID;",
+               new { productID = productID });
+
+            _connection.Execute("DELETE FROM products WHERE ProductID = @productID;",
+               new { productID = productID });
+        }
+    }
+}
